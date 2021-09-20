8b8d02a6d058455a7a3e7bfc1708e680185bec66
Jose Saint Hilaire <itt.jose.sainthilaire@gmail.com>
2021-09-20 10:02:26 -0400

    Add mapping to navigate between windows

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,11 +1,11 @@
 local utils = require('utils')
-local silentOpts = {noremap = true, silent = true} 
+local silentOpts = {noremap = true, silent = true}
 
--- nice window navigation 
-utils.map('n', '<C-h>', '<C-w>h')
-utils.map('n', '<C-j>', '<C-w>j')
-utils.map('n', '<C-k>', '<C-w>k')
-utils.map('n', '<C-l>', '<C-w>l')
+-- nice window navigation
+utils.map('n', '<Leader>h', '<C-w>h')
+utils.map('n', '<Leader>j', '<C-w>j')
+utils.map('n', '<Leader>k', '<C-w>k')
+utils.map('n', '<Leader>l', '<C-w>l')
 
 utils.map('n', '<C-c>', '<Esc>')
 
@@ -17,3 +17,9 @@ utils.map('n', '<Leader>fc', ':Telescope find_files cwd=~/.config/nvim<CR>', sil
 utils.map('n', '<Leader>fs', ':Telescope lsp_document_symbols<CR>', silentOpts)
 utils.map('n', '<Leader>fd', ':Telescope lsp_document_diagnostics<CR>', silentOpts)
 utils.map('n', '<Leader>fD', ':Telescope lsp_workspace_diagnostics<CR>', silentOpts)
+
+-- Use alt + hjkl to resize windows
+utils.map('n', '∆', ':resize -2<CR>')
+utils.map('n', '˚', ':resize +2<CR>')
+utils.map('n', '˙', ':vertical resize -2<CR>')
+utils.map('n', '¬', ':vertical resize +2<CR>')
