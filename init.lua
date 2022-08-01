ef82b5ab2f1108419479abc5c72bf5633421679a
Jose Saint Hilaire <itt.jose.sainthilaire@gmail.com>
2022-08-01 18:47:17 -0400

    use a more modern LSP tool/conf

--- init.lua
+++ init.lua
@@ -1,14 +1,7 @@
 vim.g.mapleader = ' '
 
-require('options')
 
--- Auto install packer.nvim if not exists
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd 'packadd packer.nvim'
-end
 
 if (not fn.has('gui_running')) and (vim.g['&term'] ~= 'screen' and vim.g['&term'] ~= 'tmux') then
   vim.g['&t_8f'] = "<Esc>[38;2;%lu;%lu;%lum"
@@ -16,13 +9,9 @@ if (not fn.has('gui_running')) and (vim.g['&term'] ~= 'screen' and vim.g['&term'
 end
 
 
-require('plugins')
-
-require('lsp')
-
+require "utils"
+require "options"
+require("plugins").setup()
 require('keybindings')
-
 require('autocommands')
-
-require('config.completion')
 require('config.theme')
---
+++ lua/config/lsp/init.lua
@@ -0,0 +1,128 @@
+local M = {}
+
+local servers = {
+  gopls = {},
+  html = {},
+  jsonls = {
+    settings = {
+      json = {
+        schemas = require("schemastore").json.schemas(),
+      },
+    },
+  },
+  pyright = {
+    analysis = {
+      typeCheckingMode = "off",
+    },
+  },
+  sumneko_lua = {
+    settings = {
+      Lua = {
+        runtime = {
+          version = "LuaJIT",
+          path = vim.split(package.path, ";"),
+        },
+        diagnostics = {
+          globals = { "vim", "describe", "it", "before_each", "after_each", "packer_plugins" },
+        },
+        workspace = {
+          library = {
+            [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+            [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+          },
+          maxPreload = 2000,
+          preloadFileSize = 50000,
+        },
+        completion = { callSnippet = "Both" },
+        telemetry = { enable = false },
+      },
+    },
+  },
+  tsserver = { disable_formatting = true },
+  vimls = {},
+  tailwindcss = {},
+  yamlls = {
+    schemastore = {
+      enable = true,
+    },
+    settings = {
+      yaml = {
+        hover = true,
+        completion = true,
+        validate = true,
+        schemas = require("schemastore").json.schemas(),
+      },
+    },
+  },
+  dockerls = {},
+  graphql = {},
+  bashls = {},
+  emmet_ls = {},
+  marksman = {},
+}
+
+function M.on_attach(client, bufnr)
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+  vim.api.nvim_buf_set_option(bufnr, "formatexpr", "v:lua.vim.lsp.formatexpr()")
+  require("config.lsp.keymaps").setup(client, bufnr)
+  require("config.lsp.null-ls.formatters").setup(client, bufnr)
+
+  if client.server_capabilities.definitionProvider then
+    vim.api.nvim_buf_set_option(bufnr, "tagfunc", "v:lua.vim.lsp.tagfunc")
+  end
+
+  require("aerial").on_attach(client, bufnr)
+end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.foldingRange = {
+  dynamicRegistration = false,
+  lineFoldingOnly = true,
+}
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+  properties = {
+    "documentation",
+    "detail",
+    "additionalTextEdits",
+  },
+}
+
+M.capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+local opts = {
+  on_attach = M.on_attach,
+  capabilities = M.capabilities,
+  flags = {
+    debounce_text_changes = 150,
+  },
+}
+
+require("config.lsp.handlers").setup()
+
+function M.setup()
+  require("config.lsp.null-ls").setup(opts)
+  require("config.lsp.installer").setup(servers, opts)
+end
+
+local diagnostics_active = true
+
+function M.toggle_diagnostics()
+  diagnostics_active = not diagnostics_active
+  if diagnostics_active then
+    vim.diagnostic.show()
+  else
+    vim.diagnostic.hide()
+  end
+end
+
+function M.remove_unused_imports()
+  vim.diagnostic.setqflist { severity = vim.diagnostic.severity.WARN }
+  vim.cmd "packadd cfilter"
+  vim.cmd "Cfilter /main/"
+  vim.cmd "Cfilter /The import/"
+  vim.cmd "cdo normal dd"
+  vim.cmd "cclose"
+  vim.cmd "wa"
+end
+
+return M
